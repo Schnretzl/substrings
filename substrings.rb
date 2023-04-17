ae0c985b954d8c19f79d507ced4b4182dd250b58
Wes Hendley <wrhendley@gmail.com>
2023-04-17 12:09:35 -0500

    Add test cases

--- substrings.rb
+++ substrings.rb
@@ -18,4 +18,32 @@ def substrings(input, dictionary)
     return output
 end
 
-p substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
+# Test case 1: Example input
+string = "Howdy partner, sit down! How's it going?"
+dictionary = ["how", "sit", "going"]
+puts substrings(string, dictionary)  # {"how"=>2, "sit"=>1, "going"=>1}
+
+# Test case 2: Empty string and dictionary
+string = ""
+dictionary = []
+puts substrings(string, dictionary)  # {}
+
+# Test case 3: No matches in string
+string = "The quick brown fox jumps over the lazy dog"
+dictionary = ["cat", "dog", "bird"]
+puts substrings(string, dictionary)  # {"dog"=>1}
+
+# Test case 4: Case insensitive matches
+string = "The quick brown fox JUMPS over the lazy dog"
+dictionary = ["jumps", "DOG"]
+puts substrings(string, dictionary)  # {"jumps"=>1, "DOG"=>1}
+
+# Test case 5: Multiple matches of the same substring
+string = "The cat in the hat sat on the mat"
+dictionary = ["cat", "hat", "mat"]
+puts substrings(string, dictionary)  # {"cat"=>1, "hat"=>2, "mat"=>2}
+
+# Test case 6: Substrings that are substrings of other substrings
+string = "It's raining cats and dogs"
+dictionary = ["cat", "cats", "dog", "dogs"]
+puts substrings(string, dictionary)  # {"cat"=>1, "cats"=>1, "dog"=>1, "dogs"=>1}
